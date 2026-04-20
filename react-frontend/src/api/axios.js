ceb5db5846ec9762c5686c75f5333a28598ab06b
Kaviya J <kaviyajayaraj131005@gmail.com>
2026-04-20 10:22:02 +0530

    updated files

--- react-frontend/src/api/axios.js
+++ react-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://it-documentation-portal.onrender.com/',
+  baseURL: 'https://it-documentation-portal.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
